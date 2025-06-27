32bfc65e03d3cb380b328412fdf151d93043782b
Mayra30345 <palmayra966@gmail.com>
2025-06-27 18:41:02 +0530

    Add files via upload

---
+++ Activity2.py
@@ -0,0 +1,8 @@
+x = "john"
+y = "jammy"
+print(x)
+print(y)
+codingal="codingal"
+print(codingal)
+n=input("Enter your name: ")
+print("Welcome\n",n,"codingal class")
---
+++ Printing.py
@@ -0,0 +1,7 @@
+print("Welcome to coding")
+
+print(5)
+
+print("Hello",9)
+
+print("Coding is fun",end="#")
