ca1fbe43d74474afa2acbc7274f1aa10c576838d
Mayra30345 <palmayra966@gmail.com>
2025-08-08 17:37:46 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,15 @@
+
+print("Your friend's birthday is on:  ")
+a="A's birthday is on 7 september 2013"
+b="B's birthday is on april 2013"
+c="C's birthday is on 25 april 2013"
+d="D's birthday is on august 2013"
+e="E's birthday is on 5 january 2013"
+
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
